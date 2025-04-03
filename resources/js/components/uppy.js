693694db03208609736dc11782008d422ffdcd86
Mikhail Panyushkin <mpanius@gmail.com>
2025-04-03 16:36:25 +0300

    Финализация загрузки

--- resources/js/components/uppy.js
+++ resources/js/components/uppy.js
@@ -16,6 +16,52 @@ export default function uppy({state, maxFiles, maxSize, directory, companionUrl,
         disk: disk || 's3',
 
         /**
+         * Нормализует имя файла, удаляя специальные символы и заменяя пробелы на дефисы
+         * Аналог Laravel Str::slug() для JavaScript
+         * 
+         * @param {string} filename - Имя файла для нормализации
+         * @returns {string} - Нормализованное имя файла с сохранением расширения
+         */
+        slugifyFilename(filename) {
+            // Разделяем имя файла и расширение
+            const lastDotIndex = filename.lastIndexOf('.');
+            let name = filename;
+            let extension = '';
+            
+            if (lastDotIndex !== -1) {
+                name = filename.substring(0, lastDotIndex);
+                extension = filename.substring(lastDotIndex);
+            }
+            
+            // Транслитерация кириллицы
+            const transliterationMap = {
+                'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ё': 'e',
+                'ж': 'zh', 'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 
+                'н': 'n', 'о': 'o', 'п': 'p', 'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 
+                'ф': 'f', 'х': 'h', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'sch', 'ъ': '', 
+                'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya',
+                'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
+                'Ж': 'Zh', 'З': 'Z', 'И': 'I', 'Й': 'Y', 'К': 'K', 'Л': 'L', 'М': 'M',
+                'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U',
+                'Ф': 'F', 'Х': 'H', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Sch', 'Ъ': '',
+                'Ы': 'Y', 'Ь': '', 'Э': 'E', 'Ю': 'Yu', 'Я': 'Ya'
+            };
+            
+            // Заменяем кириллические символы на латинские
+            let transliterated = name.split('').map(char => transliterationMap[char] || char).join('');
+            
+            // Преобразуем строку: заменяем неалфавитные символы на дефисы и удаляем ненужные символы
+            let slug = transliterated
+                .toLowerCase()
+                .replace(/[^\w\s-]/g, '') // Удаляем все символы, кроме букв, цифр, подчеркиваний и пробелов
+                .replace(/[\s_]+/g, '-') // Заменяем пробелы и подчеркивания на дефисы
+                .replace(/^-+|-+$/g, ''); // Удаляем начальные и конечные дефисы
+            
+            // Возвращаем нормализованное имя с расширением
+            return slug + extension.toLowerCase();
+        },
+
+        /**
          * Форматирует размер файла в человекочитаемый формат
          */
         bytesToSize(bytes) {
@@ -127,6 +173,17 @@ export default function uppy({state, maxFiles, maxSize, directory, companionUrl,
                     maxNumberOfFiles: maxFiles,
                     maxFileSize: maxSize,
                     minNumberOfFiles: 1
+                },
+                onBeforeFileAdded: (currentFile) => {
+                    // Нормализуем имя файла перед добавлением его в очередь
+                    const normalizedFilename = this.slugifyFilename(currentFile.name);
+                    console.log(`Нормализация имени файла: ${currentFile.name} -> ${normalizedFilename}`);
+                    
+                    // Возвращаем файл с измененным именем
+                    return {
+                        ...currentFile,
+                        name: normalizedFilename
+                    };
                 },
                 onBeforeUpload: (files) => {
                     const updatedFiles = {}
